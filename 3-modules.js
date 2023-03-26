d2b65514d2f28fccc72b9287182eb4294a8c0976
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-03-26 15:27:01 +0300

    modules in nodejs done

--- 3-modules.js
+++ 3-modules.js
@@ -4,8 +4,12 @@
 
 const names = require('./4-names')
 const sayHi = require('./5-utils')
+const alternativeFlavour = require('./6-alternative-flavour')
+require('./7-mind-grenade')
+
 const {john, peter} = names
 
 sayHi('Daniel')
 sayHi(john)
 sayHi(peter)
+sayHi(alternativeFlavour.singlePerson.name)
