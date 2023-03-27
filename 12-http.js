8a55036487cd2a05bf44feb825107569ff8c6d61
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-03-27 23:11:15 +0300

    http module

---
+++ 12-http.js
@@ -0,0 +1,17 @@
+const http = require('http');
+// HTTP is a built-in node.js module
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.end('Welcome to our home page')
+    }
+    if(req.url === '/about') {
+        res.end('Here is our short history')
+    }
+    res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    `)
+})
+
+server.listen(5000)
