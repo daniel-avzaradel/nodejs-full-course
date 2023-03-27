ee7d60c330f9fba661d0caa5d736b929fe115622
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-03-27 23:40:30 +0300

    added package.json through npm init -y

--- 13-npm.js
+++ 13-npm.js
@@ -7,3 +7,8 @@
 // global dependency - use it in any project
 // npm install -g <packageName>
 // sudo npm install -g <packageName> (macOS)
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties, etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
